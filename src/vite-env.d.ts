e2984fefe1b57ad5e5879712ec5730d4eaf83c21
pradeep <bunny.akki21@gmail.com>
2026-03-27 21:27:30 +0530

    added all

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string
-  // add other env vars here if needed
+  readonly VITE_API_BASE_URL: string;
+  // Add more environment variables here if needed
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
